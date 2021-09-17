a2a7570091058e16c78f658f3dabb448bce8783e
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-09-17 17:52:56 +0100

    builded the search form/engine

--- src/index.js
+++ src/index.js
@@ -37,10 +37,24 @@ iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.dat
 iconElement.setAttribute("alt", response.data.weather[0].description); 
 
 }
-
-
+function search(city) {
 let apiKey = "127e0842e2c151bd26af6c67b6803235";
-let city = "New York"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
 `
 axios.get(apiUrl).then(displayTemperature); 
+
+}
+
+function submit (event){
+    event.preventDefault(); 
+    let cityInputElement = document.querySelector("#city-input"); 
+    search(cityInputElement.value); 
+    
+}
+
+
+
+search("New York"); 
+
+let form = document.querySelector("#search-form"); 
+form.addEventListener("submit", submit); 
