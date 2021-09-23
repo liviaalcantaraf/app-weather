7c18478f364e7f792c81de352ad344b2d39213e6
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-09-23 17:14:35 +0100

    Added current location button

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-    <div class="col-2">
+    <div class="col">
      <div class="weather-forecast-date">
         ${formatDay(forecastDay.dt)}
 </div>
@@ -117,6 +117,17 @@ function submit(event) {
   search(cityInputElement.value);
 }
 
+function searchLocation(position) {
+  let apiKey = "127e0842e2c151bd26af6c67b6803235";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
 
@@ -144,10 +155,13 @@ function displayCelsiusTemperature(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", submit);
 
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Lisbon");
